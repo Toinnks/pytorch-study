008fc4d657f453f72737286dd9b414f45e68cd7b
Forward <007sun6@gmail.com>
2025-10-19 08:33:28 +0800

    10月19日的修改，删除没有必要的图片

--- imageTools.py
+++ imageTools.py
@@ -1,38 +1,65 @@
 import os
+import shutil
 
 from torch.utils.data import Dataset
 
 
 class TrainDatasetProcess(object):
-    image_end = ['png','jpg','jpeg']
-    def __init__(self, father_folder_path=None, train_folder_path=None, test_folder_path=None, valid_folder_path=None,
-                 train_images_path=None, test_images_path=None, valid_images_path=None, train_labels_path=None,
-                 test_labels_path=None, valid_labels_path=None):
-        self.father_folder_path = father_folder_path
-        self.train_folder_path = train_folder_path
-        self.test_folder_path = test_folder_path
-        self.valid_folder_path = valid_folder_path
-        self.train_images_path = train_images_path
-        self.test_images_path = test_images_path
-        self.valid_images_path = valid_images_path
-        self.train_labels_path = train_labels_path
-        self.test_labels_path = test_labels_path
-        self.valid_labels_path = valid_labels_path
+    image_end = ['png', 'jpg', 'jpeg']
+    """train_folder_path = None, test_folder_path = None, valid_folder_path = None,
+    train_images_path = None, test_images_path = None, valid_images_path = None, train_labels_path = None,
+    test_labels_path = None, valid_labels_path = None"""
+
+    def __init__(self, src_folder_path):
+        self.src_folder_path = src_folder_path
 
     def get_child_from_father(self):
         pass
 
     def remove_image_from_include_str(self, key: str):
         # 在父文件夹下的图片名中有key字符，则移除该图片
-        folder_path = self.father_folder_path
+        folder_path = self.src_folder_path
         for filename in os.listdir(folder_path):
-            if  key in filename:
+            if key in filename:
                 file_path = os.path.join(folder_path, filename)
                 os.remove(file_path)
                 print(f"已删除: {filename}")
         print(f"{folder_path}清理完成！")
 
-print()
-s1 = TrainDatasetProcess()
-s1.father_folder_path = r"D:\else\image"
-s1.remove_image_from_include_str("33")
+    def judge_folder(self, is_train: bool, is_test: bool, is_valid: bool):
+        train_child_folder = None
+        test_child_folder = None
+        valid_child_folder = None
+        child_folder = os.listdir(self.src_folder_path)
+        for i in child_folder:
+            if 'train' in i:
+                train_child_folder = os.path.join(self.src_folder_path, i)
+            if 'test' in i:
+                test_child_folder = os.path.join(self.src_folder_path, i)
+            if 'valid' in i:
+                valid_child_folder = os.path.join(self.src_folder_path, i)
+        if is_test and test_child_folder is None:
+            print("test文件夹不存在")
+
+        if is_train and train_child_folder is None:
+            print("train文件夹不存在")
+
+        if is_valid and valid_child_folder is None:
+            print("valid文件夹不存在")
+
+    def copy_folder_to_src(self, need_copy_folder: str):
+        src = self.src_folder_path
+        for item in os.listdir(need_copy_folder):
+            src_path = os.path.join(src, item)
+            dst_path = os.path.join(need_copy_folder, item)
+
+            if os.path.isdir(dst_path):
+                # 如果是文件夹，递归复制
+                shutil.copytree(dst_path, src_path, dirs_exist_ok=True)
+            else:
+                # 如果是文件，直接复制
+                shutil.copy2(dst_path, src_path)
+
+
+s1=TrainDatasetProcess(src_folder_path=r"C:\Users\sgy\Desktop\1")
+s1.copy_folder_to_src(need_copy_folder=r"C:\Users\sgy\Desktop\2")
